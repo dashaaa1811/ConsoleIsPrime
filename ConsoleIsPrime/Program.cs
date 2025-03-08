3953d486bc4a457dbc7c8870bd114e38e3bbf794
Dasha <darabrueva65@gmail.com>
2025-03-08 11:36:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleIsPrime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIsPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrime(num));
+        }
+        static bool IsPrime(int num)
+        {
+            if(num<2) return false;
+            int i = 2;
+            while (i * i >= num)
+            {
+                if(num%i == 0)
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+}
