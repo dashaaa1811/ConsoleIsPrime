b45b4bf000a1d01fc0a883849b42c5d47164a319
Dasha <darabrueva65@gmail.com>
2025-03-08 11:37:48 +0300

    добавила комментарий к задаче

--- ConsoleIsPrime/Program.cs
+++ ConsoleIsPrime/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//Определение, является ли число простым
+//Структура:
+//Входные данные: целое число number.
+//Выходные данные: true, если число простое, иначе false.
+//Простое число — это число, которое делится только на 1 и на себя (например, 2, 3, 5, 7, 11).
+
 namespace ConsoleIsPrime
 {
     internal class Program
